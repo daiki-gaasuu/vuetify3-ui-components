cc316de79361515abf3a8257ffcd341dd02f5372
daiki.s <daiki.s@itnav.co.jp>
2022-12-09 07:33:06 +0900

    add: gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vuetify3-ui-components/' : './',
   plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
