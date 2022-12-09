4f53cfff42547a56858743fb02886ffc0d1fc02c
daiki.s <daiki.s@itnav.co.jp>
2022-12-10 01:29:49 +0900

    fix: lint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,11 @@
 <script lang="ts" setup>
-const componentList = ['Icon', 'Forms']
+const componentList = ['Icon', 'Forms'];
 </script>
 <template>
   <div>
     <h2>UI Component Link List</h2>
     <v-list lines="one">
-      <v-list-item
-        v-for="item in componentList"
-        :key="item"
-        :title="item"
-        :to="{name: item}">
-      </v-list-item>
+      <v-list-item v-for="item in componentList" :key="item" :title="item" :to="{ name: item }"></v-list-item>
     </v-list>
   </div>
 </template>
--- src/views/Icon.vue
+++ src/views/Icon.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
-import { mdiAccount } from '@mdi/js'
-import viteIcon from "@/components/icons/ViteIcon.vue";
+import { mdiAccount } from '@mdi/js';
+import viteIcon from '@/components/icons/ViteIcon.vue';
 </script>
 <template>
-    <v-icon :icon="mdiAccount"></v-icon>
+  <v-icon :icon="mdiAccount"></v-icon>
   <v-icon :icon="viteIcon"></v-icon>
 </template>
